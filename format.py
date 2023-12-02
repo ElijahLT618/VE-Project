9083540ee6ab8e05bfea4e55303724b3b9efe542
Elijah Theisen <elijahltheisen@gmail.com>
2023-12-01 20:34:07 -0600

    Basic Text boxes and forms

---
+++ format.py
@@ -0,0 +1,17 @@
+markup = """
+<!doctype html>
+    <html>
+    <head>
+        <title>{0}</title>
+    </head>
+    <body>
+        <h1>{1}</h1>
+    </body>
+</html>
+"""
+
+markup = markup.format('My Page title', 'Page Heading')
+print(markup)
+
+
+#Way to change text via {}
